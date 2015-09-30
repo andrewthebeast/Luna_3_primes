71b22dd2e373a33d0aa5dee0bcc4acc33d425a8e
andrewthebeast <andrewr.luna@yahoo.com>
2015-09-30 11:00:06 -0500

    primes

---
+++ src/luna_3_primes/Luna_3_primes.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package luna_3_primes;
+
+/**
+ *
+ * @author andrewthebeast
+ */
+public class Luna_3_primes {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int evens = 0;
+        for(int i = 0; i < 100;i++){
+            }
+        /*
+        A method is a little unit 
+        that communicates with other parts 
+        of the other program by returning values
+        (most of the time
+        */
+         
+    }
+        static boolean isEven (int somenumber) {
+            return (somenumber % 2 == 0);
+        }
+        static boolean isOdd (int somenumber) {
+            return (somenumber % 2 != 0);
+    }  
+        static boolean isPrime(int number){
+                 boolean prime = true;
+                 int divisors = 0;
+                 for(int i = 2; i < number/2; i++){
+                     if(number % i == 0){
+                     prime = false;
+                     }
+                 return prime;
+ 
+                 
+        /*         
+        What is a method?
+            A method is a function
+            It is like a little program, e.g.
+            like finding a square root , something you
+            might want todo again and again 
+    */
+     
+            // A return value is the type of
+            //vale youb are giving back to the program
+          
+  if(divisor > 0){
+      return false;
+  }else{
+      
+  }
